33a203c6cefc0a2cca038af779bc5b4549953834
ksks48 <113302484+ksks48@users.noreply.github.com>
2023-06-03 17:48:18 +0300

    course

--- less_2.py
+++ less_2.py
@@ -1,7 +1,15 @@
 import numpy as np
 import tensorflow as tf
 
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('loss')<0.4):
+            print("\nLoss is low so cancelling training!")
+            self.model.stop_training = True
 
+
+
+callbacks = myCallback()
 fmnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fmnist.load_data()
 
@@ -28,6 +36,6 @@ model.compile(optimizer = tf.optimizers.Adam(),
               loss = 'sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=5)
+model.fit(train_images, train_labels, epochs=5, callbacks=[callbacks])
 
 print(model.evaluate(test_images, test_labels))
